373376db76df5bb81d9cf436776e35e00cb73405
Lord <103863076+Lord83700@users.noreply.github.com>
2024-03-07 13:02:42 +0100

    Fixed the error I created

--- Appli-SAE-master/app/src/main/java/com/example/applisae/Item.java
+++ Appli-SAE-master/app/src/main/java/com/example/applisae/Item.java
@@ -4,29 +4,9 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.Objects;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class Item{
-    
-    try {
-            JSONObject jsonObject = new JSONObject(jsonString);
 
-            // Récupérer les valeurs de psrc et pdest
-            String psrc = jsonObject.getJSONObject("transid").getString("psrc");
-            String pdest = jsonObject.getJSONObject("transid").getString("pdest");
-            String sources = jsonObject.getJSONObject("transid").getString("sources");
-            String recherche = jsonObject.getJSONObject("transid").getString("recherche");
-            String reponse = jsonObject.getJSONObject("transid").getString("reponse");
-            String ipreponse = jsonObject.getJSONObject("transid").getString("ipreponse");
-            String tempsrep = jsonObject.getJSONObject("transid").getString("tempsrep");
-
-            Log.d("MainActivity", "psrc: " + psrc);
-            Log.d("MainActivity", "pdest: " + pdest);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    
     private String _id, date, intdescript, macsrc, macdest, marque, protocole, ipsrc, ipdest, source, type;
     private Number numtrame;
 
